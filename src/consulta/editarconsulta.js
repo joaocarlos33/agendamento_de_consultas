37735ce8c3b37d6b3e0b2717bfcfec37106fc63f
joaocarlos33 <joaocarros.peixotoibiapina@gmail.com>
2024-11-29 08:07:08 -0300

    primeiro codigo de update com sucesso

--- src/consulta/editarconsulta.js
+++ src/consulta/editarconsulta.js
@@ -3,17 +3,22 @@ import { conn } from "../data/dbConnection.js";
 
 const consultRouteUpdate = Router();
 
-consultRouteUpdate.put("put/agendamento", (req, res) => {
-    const {teste , id} = req.body;
+consultRouteUpdate.put("/edit/agendamento", (req, res) => {
+    const { id, paciente ,medico , data, horario, sala } = req.body;
 
-    conn.query(``, (err, result) => {
-
-        if (err) {
-            return res.json(err.message)
+    conn.query(
+        `UPDATE agendamentos SET paciente = ${paciente}, medico = ${medico}, data = '${data}', horario = '${horario}', sala = ${sala} WHERE id = ${id}`,
+        (err, result) => {
+            if (err) {
+                res.status(500).json({ erro: "Erro ao atualizar o agendamento." });
+            } else {
+                res.json({
+                    mensagem: "Agendamento atualizado com sucesso!",
+                });
+            }
         }
+    );
+});
 
-        res.json(result)
-    })
-})
 
-export{consultRouteUpdate}
+export { consultRouteUpdate }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { consultroute } from "./consulta/novaconsulta.js";
 import { consultRouteList } from "./consulta/listagemconsulta.js";
+import { consultRouteUpdate } from "./consulta/editarconsulta.js";
 
 const app = express();
 app.use(express.json());
 app.use(consultroute);
 app.use(consultRouteList)
+app.use(consultRouteUpdate)
 
 
 app.listen(4444, () => console.log("Servidor rodando...❤️"))
