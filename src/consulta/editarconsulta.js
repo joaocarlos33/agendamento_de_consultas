10741e6413e2eb5becdb81746ff8098aec522ba0
joaocarlos33 <joaocarros.peixotoibiapina@gmail.com>
2024-11-29 09:01:26 -0300

    finalização da parte de update

--- src/consulta/editarconsulta.js
+++ src/consulta/editarconsulta.js
@@ -21,4 +21,93 @@ consultRouteUpdate.put("/edit/agendamento", (req, res) => {
 });
 
 
+consultRouteUpdate.put("/edit/medico", (req, res) => {
+    const { id , nome , crm , telefone , email , especialidade } = req.body;
+
+    conn.query(
+        `UPDATE medicos SET nome = '${nome}', crm = '${crm}', telefone = '${telefone}', email = '${email}', especialidade = '${especialidade}' WHERE id = ${id}`,
+        (err, result) => {
+            if (err) {
+                res.status(500).json({ erro: "Erro ao atualizar o agendamento." });
+            } else {
+                res.json({
+                    mensagem: "Agendamento atualizado com sucesso!",
+                });
+            }
+        }
+    );
+});
+
+
+consultRouteUpdate.put("/edit/paciente", (req, res) => {
+    const { cpf , nome , data_nascimento , telefone , email , endereco } = req.body;
+
+    conn.query(
+        `UPDATE pacientes SET nome = '${nome}', data_nascimento = '${data_nascimento}', telefone = '${telefone}', email = '${email}', endereco = '${endereco}' WHERE cpf = ${cpf} `,
+        (err, result) => {
+            if (err) {
+                res.status(500).json({ erro: "Erro ao atualizar o agendamento." });
+            } else {
+                res.json({
+                    mensagem: "Agendamento atualizado com sucesso!",
+                });
+            }
+        }
+    );
+});
+
+
+consultRouteUpdate.put("/edit/pagamento", (req, res) => {
+    const { id , data_pagamento , valor } = req.body;
+
+    conn.query(
+        `UPDATE pagamentos SET data_pagamento = '${data_pagamento}', valor = '${valor}' WHERE id = ${id} `,
+        (err, result) => {
+            if (err) {
+                res.status(500).json({ erro: "Erro ao atualizar o agendamento." });
+            } else {
+                res.json({
+                    mensagem: "Agendamento atualizado com sucesso!",
+                });
+            }
+        }
+    );
+});
+
+
+consultRouteUpdate.put("/edit/sala", (req, res) => {
+    const { id , numero , andar , tipo } = req.body;
+
+    conn.query(
+        `UPDATE salas SET numero = ${numero}, andar = ${andar} , tipo = '${tipo}' WHERE id = ${id} `,
+        (err, result) => {
+            if (err) {
+                res.status(500).json({ erro: "Erro ao atualizar o agendamento." });
+            } else {
+                res.json({
+                    mensagem: "Agendamento atualizado com sucesso!",
+                });
+            }
+        }
+    );
+});
+
+
+consultRouteUpdate.put("/edit/tipodeconsulta", (req, res) => {
+    const { id , descricao } = req.body;
+
+    conn.query(
+        `UPDATE tipoconsulta SET descricao = '${descricao}' WHERE id = ${id} `,
+        (err, result) => {
+            if (err) {
+                res.status(500).json({ erro: "Erro ao atualizar o agendamento." });
+            } else {
+                res.json({
+                    mensagem: "Agendamento atualizado com sucesso!",
+                });
+            }
+        }
+    );
+});
+
 export { consultRouteUpdate }
